6e8d03abb6258cb34d773aa883e7a5a0f488d846
artem <mikyedit@gmail.com>
2024-02-01 12:53:24 +0300

    added new line

--- test_web.py
+++ test_web.py
@@ -9,6 +9,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 def chromedriver():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
     driver = webdriver.Chrome(options=options)
     return driver
 
